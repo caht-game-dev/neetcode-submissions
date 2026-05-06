0d635e922db70b97860bd64e1955cad189873975
Caht <caht205@gmail.com>
2026-05-06 23:06:55 +0700

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) 
+    {
+        int n = nums.Length;
+        int[] res = new int[n];
+        Array.Fill(res, 1);
+
+        int prefix = 1;
+        for (int i = 0; i < n; i++)
+        {
+            res[i] *= prefix;
+            prefix *= nums[i];
+        }
+
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            res[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return res;
+    }
+}
